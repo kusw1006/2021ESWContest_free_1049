31c6c1f28138fbadf5858d432a68bb47b128d093
ChanHyun Lee <ech97@konkuk.ac.kr>
2021-09-20 20:47:31 +0900

    relocation & add spacing

--- kusw_TTS.py
+++ kusw_TTS.py
@@ -1,54 +1,52 @@
-'''
-sudo apt-get install ffmpeg
-python3 -m pip install pydub 
-python3로 실행해야함
-'''
 import os
 import sys
 import socket
 import threading
 
+import time
 import io
 from io import BytesIO
 
 from pydub import AudioSegment
 from pydub.playback import play
- 
+
+
+HOST = '114.70.22.237'
+PORT = 5053
 
 def write_to_fp(fp, input_wav):
     fp.write(input_wav)
 
-
 def recv(client_sock):
     fp = BytesIO()
 
+    id = 0
+    seekInd = 0
+    
     while True:
+        #fp = open("./01.wav", 'ab')
         recv = client_sock.recv(1024)  
 
         if len(recv):
+            if id == 0:
+                print(recv)
             write_to_fp(fp, recv)
-            print(len(recv),len(fp.read()))
 
-        if fp.read and len(recv)==1:       
-            print("aaa",len(recv),len(fp.read()))
-            print(fp.read())
+            #fp.close()
+            lenRecv = len(recv)
+            print(lenRecv)
+            id += lenRecv
 
+        if len(recv) > 1 and len(recv) < 1000:
             fp.seek(0)
-
-            #print(fp.read())
-
-            song = AudioSegment.from_file(fp, format="wav")
+            song = AudioSegment.from_wav(fp)
+            print("PPPPPPPPPPPPPPPPPPPPlayyyyy")
             play(song)
- 
+
 
 #-------------이부분만 반복--------------#
-import time
 
 
-HOST = '114.70.22.237'
-PORT = 5052
-
- 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((HOST,PORT))
 
