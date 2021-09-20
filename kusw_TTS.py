87e65bbf21ad7a82c3948e7b5467489167fb36dc
ChanHyun Lee <ech97@konkuk.ac.kr>
2021-09-20 21:57:01 +0900

    modify kusw_TTS.py

--- kusw_TTS.py
+++ kusw_TTS.py
@@ -7,15 +7,6 @@ import time
 import io
 from io import BytesIO
 
-<<<<<<< HEAD
-
-
-import base64
-
- 
-
-=======
->>>>>>> 0dcaccf001ac440cb00401213d080be47961cf90
 from pydub import AudioSegment
 from pydub.playback import play
 
@@ -28,14 +19,7 @@ def write_to_fp(fp, input_wav):
 
 def recv(client_sock):
     fp = BytesIO()
-<<<<<<< HEAD
     id = 0
-=======
-
-    id = 0
-    seekInd = 0
-    
->>>>>>> 0dcaccf001ac440cb00401213d080be47961cf90
     while True:
         #fp = open("./01.wav", 'ab')
         recv = client_sock.recv(1024)  
@@ -45,7 +29,6 @@ def recv(client_sock):
                 print(recv)
             write_to_fp(fp, recv)
 
-<<<<<<< HEAD
             print(len(recv),len(fp.read()))
             id += 1
 
@@ -56,12 +39,6 @@ def recv(client_sock):
             print(fp.read())
 
             
-=======
-            #fp.close()
-            lenRecv = len(recv)
-            print(lenRecv)
-            id += lenRecv
->>>>>>> 0dcaccf001ac440cb00401213d080be47961cf90
 
         if len(recv) > 1 and len(recv) < 1000:
             fp.seek(0)
@@ -72,22 +49,6 @@ def recv(client_sock):
 
 #-------------이부분만 반복--------------#
 
-<<<<<<< HEAD
-import time
-
- 
-
- 
-
-HOST = '114.70.22.237'
-
-PORT = 5053
-
- 
-
- 
-=======
->>>>>>> 0dcaccf001ac440cb00401213d080be47961cf90
 
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client_socket.connect((HOST,PORT))
